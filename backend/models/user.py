9e85813328596c8d36cb33bda155cd81c3dc15a0
mahdi <alojoharymahdi@gmail.com>
2026-09-06 20:14:52 +0300

    Add database model relationships

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,4 +1,6 @@
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+
 from database.database import Base
 
 
@@ -9,3 +11,6 @@ class User(Base):
     username = Column(String, unique=True, nullable=False)
     email = Column(String, unique=True, nullable=False)
     password = Column(String, nullable=False)
+
+    projects = relationship("Project", back_populates="owner")
+    tasks = relationship("Task", back_populates="owner")
