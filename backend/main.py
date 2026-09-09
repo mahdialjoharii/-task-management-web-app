c2a79d5d63994185cae17bafb5b54905ce499931
mahdi <alojoharymahdi@gmail.com>
2026-09-09 15:15:53 +0300

    Connect frontend login with backend authentication

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,22 @@
 from fastapi import FastAPI
-
 from database.database import Base, engine
 from models import user, project, task
 from routers.tasks import router as tasks_router
 from routers.users import router as users_router
 from routers.projects import router as projects_router
-
+from fastapi.middleware.cors import CORSMiddleware
+ 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.include_router(tasks_router)
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,79 @@
+import { useState } from "react"
 import "./App.css"
 
 function App() {
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [loggedIn, setLoggedIn] = useState(false)
+  const handleLogin = async (event) => {
+  event.preventDefault()
+
+  const formData = new URLSearchParams()
+  formData.append("username", username)
+  formData.append("password", password)
+
+  try {
+    const response = await fetch("http://127.0.0.1:8000/users/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: formData,
+    })
+
+    const data = await response.json()
+
+    console.log("Status:", response.status)
+    console.log("Response:", data)
+
+    if (response.ok) {
+  localStorage.setItem("token", data.access_token)
+  setLoggedIn(true)
+  const meResponse = await fetch("http://127.0.0.1:8000/users/me", {
+    headers: {
+      Authorization: `Bearer ${data.access_token}`,
+    },
+  })
+
+  const meData = await meResponse.json()
+
+  console.log("Current user:", meData)
+}
+  } catch (error) {
+    console.error("Login error:", error)
+  }
+}
+
+  if (loggedIn) {
   return (
-    <div className="login-page">
+    <div>
+      <h1>Dashboard</h1>
+      <p>Welcome to Task Management!</p>
+    </div>
+  )
+}
+
+return (
+  <div className="login-page">
       <div className="login-card">
         <h1>Task Management</h1>
         <p className="subtitle">Sign in to manage your tasks</p>
 
-        <form>
+        <form onSubmit={handleLogin}>
           <label>Username</label>
           <input
             type="text"
             placeholder="Enter your username"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
           />
 
           <label>Password</label>
           <input
             type="password"
             placeholder="Enter your password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
           />
 
           <button type="submit">Login</button>
